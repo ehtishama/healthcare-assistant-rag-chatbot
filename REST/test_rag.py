6d3557895912697d8215bb10d622fdd4ba51c2eb
ehtishama <ehtishamhassan9@gmail.com>
2025-01-05 00:42:01 +0000

    Use mongodb atlas to reterive documents

--- REST/test_rag.py
+++ REST/test_rag.py
@@ -1,3 +1,3 @@
 from nhs_rag import invoke_rag
 
-print( invoke_rag("I am having some backpain, what could it be?") )
+print( invoke_rag("I am having some difficulty while eating due to pain in right jaw, what could it be?") )
